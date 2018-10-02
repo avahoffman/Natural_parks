7fb2851e9ae64c9dadf01ae495ad2c1be8baa5f1
Ava Hoffman <avamariehoffman@gmail.com>
2018-10-02 00:14:32 -0600

    refine time series model

--- do_timeseries.py
+++ do_timeseries.py
@@ -9,27 +9,20 @@ def do_timeseries(NP_sub):
 	data_forecast = data_prophet.make_future_dataframe(periods=12 * 3, freq='M')
 	data_forecast = data_prophet.predict(data_forecast)
 	# maximum temperature
-	combined_maxtemp = NP_sub.rename(columns={'mergedate': 'ds', 'MaxT': 'y'})
-	data_prophet_maxtemp = fbprophet.Prophet(changepoint_prior_scale=0.05, daily_seasonality=False, weekly_seasonality=False)
-	data_prophet_maxtemp.fit(combined_maxtemp)
-	data_forecast_maxtemp = data_prophet_maxtemp.make_future_dataframe(periods=12 * 3, freq='M')
-	data_forecast_maxtemp = data_prophet_maxtemp.predict(data_forecast_maxtemp)
+	combined_maxtemp = combined_vals.loc[:,('Month','MaxT')]
+	combined_maxtemp = combined_maxtemp.groupby(['Month']).mean()
+	combined_maxtemp = pd.concat([combined_maxtemp.reset_index(drop=True), combined_maxtemp.reset_index(drop=True), combined_maxtemp.reset_index(drop=True)], axis=0)
 	# minimum temperature
-	combined_mintemp = NP_sub.rename(columns={'mergedate': 'ds', 'MinT': 'y'})
-	data_prophet_mintemp = fbprophet.Prophet(changepoint_prior_scale=0.05, daily_seasonality=False, weekly_seasonality=False)
-	data_prophet_mintemp.fit(combined_mintemp)
-	data_forecast_mintemp = data_prophet_mintemp.make_future_dataframe(periods=12 * 3, freq='M')
-	data_forecast_mintemp = data_prophet_mintemp.predict(data_forecast_mintemp)
+	combined_mintemp = combined_vals.loc[:,('Month','MinT')]
+	combined_mintemp = combined_mintemp.groupby(['Month']).mean()
+	combined_mintemp = pd.concat([combined_mintemp.reset_index(drop=True), combined_mintemp.reset_index(drop=True), combined_mintemp.reset_index(drop=True)], axis=0)
 	# cleaning
 	predicted_vals = data_forecast.drop(data_forecast.index[:len(data_forecast)-36])
 	predicted_vals = predicted_vals.loc[:,['ds','yhat']]
 	predicted_vals['date'] = predicted_vals['ds'].apply(lambda x: x + timedelta(days=1))
-	predicted_vals_maxtemp = data_forecast_maxtemp.drop(data_forecast_maxtemp.index[:len(data_forecast_maxtemp)-36])
-	predicted_vals_maxtemp = predicted_vals_maxtemp.loc[:,['ds','yhat']]
-	predicted_vals_mintemp = data_forecast_mintemp.drop(data_forecast_mintemp.index[:len(data_forecast_mintemp)-36])
-	predicted_vals_mintemp = predicted_vals_mintemp.loc[:,['ds','yhat']]
-	result_pre = pd.merge(predicted_vals, predicted_vals_maxtemp, on='ds')
-	result_vals = pd.merge(result_pre, predicted_vals_mintemp, on='ds')
+	predicted_vals_maxtemp = combined_maxtemp.loc[:,['MaxT']]
+	predicted_vals_mintemp = combined_mintemp.loc[:,['MinT']]
+	result_vals = pd.concat([predicted_vals.reset_index(drop=True), predicted_vals_maxtemp.reset_index(drop=True), predicted_vals_mintemp.reset_index(drop=True)], axis=1)
 	result_vals['month'] = result_vals['date'].apply(lambda x: x.strftime('%B'))
 	result_vals['year'] = result_vals['date'].apply(lambda x: x.strftime('%Y'))
 	return result_vals
--- euclidean_dist.py
+++ euclidean_dist.py
@@ -3,19 +3,19 @@ def euclidean_dist(result_vals,crowd_importance,min_importance,max_importance,SE
     import scipy
     from sklearn import preprocessing
     result = result_vals.drop(result_vals[result_vals['date'] < '2018-10-01' ].index)
-    cos_df = result.loc[:,('yhat_x','yhat_y',"yhat")]
+    cos_df = result.loc[:,('yhat','MaxT',"MinT")]
     crowd_imp = crowd_importance
     crowd = 10-crowd_imp
     min_imp = min_importance
     max_imp = max_importance
-    least_crowded = min(result['yhat_x'])
-    most_crowded = max(result['yhat_x'])
+    least_crowded = min(result['yhat'])
+    most_crowded = max(result['yhat'])
     optimal_crowd = least_crowded + (((most_crowded - least_crowded) / 10 )*crowd)
     weights = [crowd_importance,max_importance,min_importance]
     cos_array = cos_df.values
     ss = preprocessing.StandardScaler().fit(cos_array)
     features_std = ss.transform(cos_array)
-    user_input = pd.DataFrame([[ optimal_crowd, SELECTED_MAXTEMP, SELECTED_MINTEMP]], columns=('yhat_x','yhat_y','yhat')) 
+    user_input = pd.DataFrame([[ optimal_crowd, SELECTED_MAXTEMP, SELECTED_MINTEMP]], columns=('yhat_x','MaxT','MinT')) 
     user_input = user_input.values
     user_input = ss.transform(user_input)
     euc_dist= scipy.spatial.distance.cdist(user_input, features_std, metric='euclidean', w=weights)[0]
